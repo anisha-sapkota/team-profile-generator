50e1e1832e0edc998dbd0c6ace77ebd509e88063
Anisha Sapkota <anisha.sapkota79@gmail.com>
2022-09-30 21:06:51 +1000

    Added test cases for employee class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,21 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
-  const john = new Employee("John", "john@email.com", 1);
+  const john = new Employee("John", "john@example.com", 1);
 
   test("getName returns the name of the employee", () => {
     expect(john.getName()).toBe("John");
+  });
+
+  test("getEmail returns the email of the employee", () => {
+    expect(john.getEmail()).toBe("john@example.com");
+  });
+
+  test("getId returns the id of the employee", () => {
+    expect(john.getId()).toBe(1);
+  });
+
+  test("getRole returns Employee", () => {
+    expect(john.getRole()).toBe("Employee");
   });
 });
